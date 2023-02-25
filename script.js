ef22c4d0f2eb233d4b693b44e3425b499e42bd1d
Vainer <57346251+Vainer-prog@users.noreply.github.com>
2023-02-25 18:14:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const downloadBtn = document.getElementById("download-btn");
-const urlInput = document.getElementById("url");
+const citySelect = document.getElementById('city-select');
+const timer = document.getElementById('timer');
+const timeOffsets = {
+  london: 0,
+  moscow: 3,
+  'new-york': -5,
+  tokyo: 9,
+};
 
-downloadBtn.addEventListener("click", () => {
-    const url = urlInput.value;
-    const quality = document.querySelector('input[name="quality"]:checked').value;
-    downloadVideo(url, quality);
-});
-
-function downloadVideo(url, quality) {
-    const a = document.createElement("a");
-    a.href = `https://www.y2mate.com/mates/${url}/download/${quality}`;
-    a.download = "video.mp4";
-    a.click();
+function updateTime() {
+  const now = new Date();
+  const city = citySelect.value;
+  const offset = timeOffsets[city];
+  const cityTime = new Date(now.getTime() + offset * 60 * 60 * 1000);
+  const hours = cityTime.getHours().toString().padStart(2, '0');
+  const minutes = cityTime.getMinutes().toString().padStart(2, '0');
+  const seconds = cityTime.getSeconds().toString().padStart(2, '0');
+  timer.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  updateTime();
+  setInterval(updateTime, 1000);
+});
