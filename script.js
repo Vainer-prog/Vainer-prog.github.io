9328db9f514a3dc8e0479ffbffbbc056f5c152de
Vainer <57346251+Vainer-prog@users.noreply.github.com>
2023-02-25 18:23:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const messageInput = document.getElementById('message-input');
-const sendButton = document.getElementById('send-button');
-const chatHistory = document.querySelector('.chat-history');
+const dinosaur = document.getElementById("dinosaur");
+const cactus = document.getElementById("cactus");
 
-sendButton.addEventListener('click', () => {
-  if (messageInput.value !== '') {
-    const message = document.createElement('div');
-    message.classList.add('chat-message');
-    message.textContent = messageInput.value;
-    chatHistory.appendChild(message);
-    messageInput.value = '';
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    jump();
   }
 });
 
-messageInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    sendButton.click();
+function jump() {
+  let position = 0;
+  let timerId = setInterval(() => {
+    if (position === 150) {
+      clearInterval(timerId);
+      let downTimerId = setInterval(() => {
+        if (position === 0) {
+          clearInterval(downTimerId);
+        }
+        position -= 10;
+        dinosaur.style.bottom = position + "px";
+      }, 20);
+    }
+    position += 10;
+    dinosaur.style.bottom = position + "px";
+  }, 20);
+}
+
+let isAlive = setInterval(() => {
+  let dinosaurBottom = parseInt(
+    window.getComputedStyle(dinosaur).getPropertyValue("bottom")
+  );
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinosaurBottom <= 60) {
+    alert("Game over!");
+    clearInterval(isAlive);
   }
-});
+}, 10);
