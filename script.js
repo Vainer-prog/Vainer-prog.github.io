2d75b1358e458b8bc383caf2a89a6d97c34c96c5
Vainer <57346251+Vainer-prog@users.noreply.github.com>
2023-02-25 18:21:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-const citySelect = document.getElementById('city-select');
-const timer = document.getElementById('timer');
-const timeOffsets = {
-  london: 0,
-  moscow: 3,
-  'new-york': -5,
-  tokyo: 9,
-};
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const chatHistory = document.querySelector('.chat-history');
 
-function updateTime() {
-  const now = new Date();
-  const city = citySelect.value;
-  const offset = timeOffsets[city];
-  const cityTime = new Date(now.getTime() + offset * 60 * 60 * 1000);
-  const hours = cityTime.getHours().toString().padStart(2, '0');
-  const minutes = cityTime.getMinutes().toString().padStart(2, '0');
-  const seconds = cityTime.getSeconds().toString().padStart(2, '0');
-  timer.textContent = `${hours}:${minutes}:${seconds}`;
-}
+sendButton.addEventListener('click', () => {
+  if (messageInput.value !== '') {
+    const message = document.createElement('div');
+    message.classList.add('chat-message');
+    message.textContent = messageInput.value;
+    chatHistory.appendChild(message);
+    messageInput.value = '';
+  }
+});
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  updateTime();
-  setInterval(updateTime, 1000);
+messageInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    sendButton.click();
+  }
 });
