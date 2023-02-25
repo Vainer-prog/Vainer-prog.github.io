073ba88c1e4311ba41b7f2d6b4d507bbce1b0cf0
Vainer <57346251+Vainer-prog@users.noreply.github.com>
2023-02-25 17:55:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const form = document.getElementById("download-form");
-const resultDiv = document.getElementById("result");
+const downloadForm = document.getElementById('download-form');
+const videoPlayer = document.getElementById('video-player');
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const videoUrl = document.getElementById("video-url").value;
-  const videoQuality = document.getElementById("video-quality").value;
-  resultDiv.innerHTML = "Загрузка видео...";
-  fetch(`/download?video_url=${videoUrl}&video_quality=${videoQuality}`)
-    .then((response) => response.text())
-    .then((data) => {
-      resultDiv.innerHTML = data;
-    });
+downloadForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const url = document.getElementById('url').value;
+    const quality = document.getElementById('quality').value;
+    fetch('/download', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `url=${url}&quality=${quality}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        videoPlayer.src = data.url;
+        videoPlayer.poster = '';
+        videoPlayer.play();
+    })
+    .catch(error => console.error(error));
 });
